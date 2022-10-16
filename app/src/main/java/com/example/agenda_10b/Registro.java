462767999de066818d3592eb8e79adbab02a97bf
CuauhtemocRmz <cuauhtemocrmz@outlook.com>
2022-10-16 11:45:16 -0500

    actividades login, registro

---
+++ app/src/main/java/com/example/agenda_10b/Registro.java
@@ -0,0 +1,14 @@
+package com.example.agenda_10b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
