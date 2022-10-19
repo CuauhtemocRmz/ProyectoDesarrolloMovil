08582efe40aedc2a90143d65f1cb8384ff2d5fcc
CuauhtemocRmz <cuauhtemocrmz@outlook.com>
2022-10-18 19:39:12 -0500

    cambios para registrar usuario cambios minimos de escritura

--- app/src/main/java/com/example/agenda_10b/Pantalla_De_Carga.java
+++ app/src/main/java/com/example/agenda_10b/Pantalla_De_Carga.java
@@ -6,17 +6,35 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Pantalla_De_Carga extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_de_carga);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         int Tiempo = 3000;
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(Pantalla_De_Carga.this,MainActivity.class));
+
+        new Handler().postDelayed(() -> VerificarUsuario(),Tiempo);
+    }
+
+    private void VerificarUsuario(){
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if (firebaseUser == null){
+            startActivity(new Intent(Pantalla_De_Carga.this, MainActivity.class));
             finish();
-        },Tiempo);
+
+        }else {
+            startActivity(new Intent(Pantalla_De_Carga.this, MenuPrincipal.class ));
+            finish();
+        }
     }
 }
