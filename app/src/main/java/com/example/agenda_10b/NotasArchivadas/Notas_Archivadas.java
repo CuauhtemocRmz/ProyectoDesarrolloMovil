7f76725f5b5f077af676facf6c057098deeeddb1
Kawaiski <brayangg@live.com.mx>
2022-10-23 22:32:03 -0500

    Menu Principal # 4 - Completo

---
+++ app/src/main/java/com/example/agenda_10b/NotasArchivadas/Notas_Archivadas.java
@@ -0,0 +1,16 @@
+package com.example.agenda_10b.NotasArchivadas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agenda_10b.R;
+
+public class Notas_Archivadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas_archivadas);
+    }
+}
