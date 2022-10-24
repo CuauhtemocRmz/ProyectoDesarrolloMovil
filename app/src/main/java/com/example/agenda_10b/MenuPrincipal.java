b393fa68145c7725e79e5b8edb07dec33fd47eec
Kawaiski <brayangg@live.com.mx>
2022-10-23 21:27:54 -0500

    Menu Principal # 2

--- app/src/main/java/com/example/agenda_10b/MenuPrincipal.java
+++ app/src/main/java/com/example/agenda_10b/MenuPrincipal.java
@@ -1,15 +1,27 @@
 package com.example.agenda_10b;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -17,16 +29,75 @@ public class MenuPrincipal extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
 
+    TextView NombresPrincipal, CorreoPrincipal;
+    ProgressBar progressBarDatos;
+
+    DatabaseReference Usuarios;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("agenda_10b");
+
+        NombresPrincipal = findViewById(R.id.NombresPrincipal);
+        CorreoPrincipal = findViewById(R.id.CorreoPrincipal);
+        progressBarDatos = findViewById(R.id.ProgressBarDatos);
+        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
         CerrarSesion = findViewById(R.id.CerrarSesion);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
 
         CerrarSesion.setOnClickListener(view -> CerrarSesion());
+    }
+
+    @Override
+    protected void onStart(){
+        ComprobarInicioSesion();
+        super.onStart();
+    }
+
+    private void ComprobarInicioSesion(){
+        if (user!=null){
+            //Si el usuario a iniciado sesion
+            CargaDeDatos();
+        }
+        else{
+            //Dirigir al MainActivity
+            startActivity(new Intent(MenuPrincipal.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    private void CargaDeDatos(){
+        Usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Si los datos del usuario existe
+            if (snapshot.exists()){
+                progressBarDatos.setVisibility(View.GONE);
+                //Se mostraran los textview
+                NombresPrincipal.setVisibility(View.VISIBLE);
+                CorreoPrincipal.setVisibility(View.VISIBLE);
+
+                //obtener datos
+                String nombres = ""+snapshot.child("nombres").getValue();
+                String correo = ""+snapshot.child("correo").getValue();
+
+                //Setear los datos en los respectivos textview
+                NombresPrincipal.setText(nombres);
+                CorreoPrincipal.setText(correo);
+
+            }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void CerrarSesion() {
