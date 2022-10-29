8cecd3b213815d80070ee317959ea97394580d71
Kevin Omar Martinez <kevinomar773@gmail.com>
2022-10-29 16:01:01 -0500

    Seción 9

--- app/src/main/java/com/example/agenda_10b/ListarNotas/Listar_Notas.java
+++ app/src/main/java/com/example/agenda_10b/ListarNotas/Listar_Notas.java
@@ -1,16 +1,101 @@
 package com.example.agenda_10b.ListarNotas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.agenda_10b.Objetos.Nota;
+import com.example.agenda_10b.ViewHolder.ViewHolder_Nota;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.redsystem.agendaonLine.Objetos.Nota;
+import com.redysystem.agendaonline.R;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.agenda_10b.R;
 
 public class Listar_Notas extends AppCompatActivity {
 
+    RecyclerView recyclerViewNotas;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference BASE_DE_DATOS;
+
+    LinearLayoutManager linearLayoutManager;
+
+    FirebaseRecyclerAdapter<Nota, ViewHolder_Nota<Nota, ViewHolder_Nota> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Nota> options;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_notas);
+
+        recyclerViewNotas = findViewById(R.id.recyclerviewNotas);
+        recyclerViewNotas.setHasFixedSize(true);
+
+        firebaseDatabase = firebaseDatabase.getInstance();
+        BASE_DE_DATOS = firebaseDatabase.getReference("Notas_Publicadas");
+        ListarNotasUsuarios();
+    }
+
+    private void ListarNotasUsuarios(){
+        options = new FirebaseRecyclerOptions.Builder<Nota>().setQuery(BASE_DE_DATOS, Nota.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Nota, ViewHolder_Nota>(options) {
+            @Override
+            protected void onBlindViewHolder(@NonNull ViewHolder_Nota viewHolder_nota, int position, @NonNull Nota nota) {
+                viewHolder_nota.SetearDatos(
+                        getApplicationContext(),
+                        nota.getId_nota(),
+                        nota.getUid_usuario(),
+                        nota.getCorreo_usuario(),
+                        nota.getFecha_hora_actual(),
+                        nota.getTitulo(),
+                        nota.getDescripcion(),
+                        nota.getFecha_nota(),
+                        nota.getEstado()
+                );
+            }
+
+            @Override
+            public viewHolder_Nota onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nota,parent, false);
+                ViewHolder_Nota viewHolder_nota = new ViewHolder_Nota(view);
+                ViewHolder_Nota.setOnClickListener(new ViewHolder_Nota.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "on item click", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "on item click", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return viewHolder_nota;
+            }
+        };
+
+        linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL, false);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        recyclerViewNotas.setLayoutManager(linearLayoutManager);
+        recyclerViewNotas.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(firebaseRecyclerAdapter!=null){
+            firebaseRecyclerAdapter.startListening();
+        }
     }
 }
