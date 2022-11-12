52533782309325845837129cccacf9d6138407c0
Kawaiski <98301595+Kawaiski@users.noreply.github.com>
2022-11-11 22:06:45 -0600

    Actualizacion detalle contacto

---
+++ app/src/main/java/com/example/agenda_10b/Contactos/Detalle_Cotacto.java
@@ -0,0 +1,16 @@
+package com.example.agenda_10b.Contactos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agenda_10b.R;
+
+public class Detalle_Cotacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_cotacto);
+    }
+}
