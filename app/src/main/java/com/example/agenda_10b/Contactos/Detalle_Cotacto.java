278a63478a3e2a1500c8384fdf822f2644146f05
Kawaiski <brayangg@live.com.mx>
2022-11-11 22:40:21 -0600

    detalles de los conctactos

--- app/src/main/java/com/example/agenda_10b/Contactos/Detalle_Cotacto.java
+++ app/src/main/java/com/example/agenda_10b/Contactos/Detalle_Cotacto.java
@@ -11,6 +11,6 @@ public class Detalle_Cotacto extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle_cotacto);
+        setContentView(R.layout.activity_detalle_contacto);
     }
 }
